da97b8be2d90c66822608dd9c18dd67eccfc7184
IMSuperkaka <1072815089@qq.com>
2020-08-13 10:56:49 +0800

    assets

--- src/app.config.js
+++ src/app.config.js
@@ -29,18 +29,26 @@ module.exports.ali = {
 module.exports.wechat = {
     pages,
     tabBar: {
+        color: '#666666',
+        selectedColor: '#FF6345',
         borderStyle: 'white',
         list: [
             {
                 pagePath: 'pages/home/index',
+                iconPath: 'images/tab_icon_home_default@2x.png',
+                selectedIconPath: 'images/tab_icon_home_pressed@2x.png',
                 text: '首页'
             },
             {
                 pagePath: 'pages/coupon/index',
+                iconPath: 'images/tab_icon_coupons_default@2x.png',
+                selectedIconPath: 'images/tab_icon_coupons_pressed@2x.png',
                 text: '优惠券'
             },
             {
                 pagePath: 'pages/my/index',
+                iconPath: 'images/tab_icon_my_default@2x.png',
+                selectedIconPath: 'images/tab_icon_my_pressed@2x.png',
                 text: '我的'
             }
         ]
@@ -48,7 +56,7 @@ module.exports.wechat = {
     window: {
         navigationBarBackgroundColor: '#fff',
         navigationBarTextStyle: 'black',
-        backgroundColor: '#f5f5f5'
+        backgroundColor: '#F6F6F6'
     },
 };
 
